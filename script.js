754c2a8b2363b76ee5a2a951bef3e2466f938979
Felix <137139123+felixjr2121@users.noreply.github.com>
2023-09-07 12:22:56 -0500

    Add the playRound function
    Addition of playRound function with conditional switch statement.
    Function runs successfully once, but computerSelection variable doesn't
    change on succeeding runs.

--- script.js
+++ script.js
@@ -5,6 +5,59 @@ function getComputerChoice() {
     return choices[randomIndex];
 }
 
+function playRound(playerSelection, computerSelection) {
+    console.log(playerSelection);
+    console.log(computerSelection);
+    switch (true) {
+        case (playerSelection == "rock" && computerSelection == "scissors"):
+            alert("You won! Rock beats scissors!");
+            break;
+
+        case (playerSelection == "paper" && computerSelection == "rock"):
+            alert("You won! Paper beats rock!");
+            break;
+
+         case (playerSelection == "scissors" && computerSelection == "paper"):
+            alert("You won! Scissors beats paper!");
+            break;
+        
+        case (playerSelection == "rock" && computerSelection == "paper"):
+            alert("You lost! Rock loses to paper!");
+            break;
+
+        case (playerSelection == "paper" && computerSelection == "scissors"):
+            alert("You lost! Paper loses to scissors!");
+            break;
+
+        case (playerSelection == "scissors" && computerSelection == "rock"):
+            alert("You lost! Scissors loses to rock!");
+            break;
+
+        case (playerSelection == computerSelection):
+            alert("It's a tie!");
+            break;
+
+        default:
+            alert("You must enter rock, paper, or scissors.");
+    }
+    
+    //if (player chooses rock and comp chooses scissors) or (player chooses paper and comp chooses rock) or (player chooses scissors and comp chooses paper)
+        //return a winning message
+    //otherwise if (player chooses rock and comp chooses paper) or (player chooses paper and comp chooses scissors) or (player chooses scissors and comp chooses rock)
+        //return a losing message
+    //otherwise if player and comp make the same choice
+        //return a tie message
+    //otherwise return a message letting the player know that they must choose rock, paper, or scissors
+  }
+
+ const computerSelection = getComputerChoice();
+ const playerSelection = "paper"; 
+
+
+
+
+
+
 //function playRound(compChoice, userChoice) {
 //    if ((userChoice === "rock" && compChoice === "scissors") || (userChoice === "scissors" && compChoice == "paper") || (userChoice === "paper" && compChoice === "rock")) {
 //        return "You won the round!";
