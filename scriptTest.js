f553b2e194293880bdabdf6e150393521828cb59
Felix <137139123+felixjr2121@users.noreply.github.com>
2023-09-20 14:31:34 -0500

    Erase most code in scriptTest
    Starting over. All code except getComputerChoice function erased in
    scriptTest.

--- scriptTest.js
+++ scriptTest.js
@@ -3,44 +3,21 @@ function getComputerChoice() {
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
 }
-const computerSelection = getComputerChoice();
 
 const rock = document.querySelector(".rock");
 const paper = document.querySelector(".paper");
 const scissors = document.querySelector(".scissors");
 
-rock.addEventListener("click", function() {let playerSelection = "rock";});
-paper.addEventListener("click", function() {let playerSelection = "paper";});
-scissors.addEventListener("click", function() {let playerSelection = "scissors";});
+const computerSelection = getComputerChoice();
 
-function round(playerSelection, computerSelection) {
-    switch (true) {
-        case (playerSelection == "rock" && computerSelection == "scissors"):
-            console.log("You won! Rock beats scissors!");
-            break;
-
-        case (playerSelection == "paper" && computerSelection == "rock"):
-            console.log("You won! Paper beats rock!");
-            break;
-
-        case (playerSelection == "scissors" && computerSelection == "paper"):
-            console.log("You won! Scissors beats paper!");
-            break;
-    
-        case (playerSelection == "rock" && computerSelection == "paper"):
-            console.log("You lost! Rock loses to paper!");
-            break;
-
-        case (playerSelection == "paper" && computerSelection == "scissors"):
-            console.log("You lost! Paper loses to scissors!");
-            break;
-
-        case (playerSelection == "scissors" && computerSelection == "rock"):
-            console.log("You lost! Scissors loses to rock!");
-            break;
-
-        case (playerSelection == computerSelection):
-            console.log("It's a tie!");
-            break;
+function rockRound(computerSelection) {
+    if (computerSelection == "rock") {
+        console.log("The computer chose rock! It's a tie!");
+    } else if (computerSelection == "paper") {
+        console.log("The computer chose paper! You lose :(");
+    } else if (computerSelection == "scissors") {
+        console.log("The computer chose scissors. You win!");
     }
 }
+
+rock.addEventListener("click", rockRound);
