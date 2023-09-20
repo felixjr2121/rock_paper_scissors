5ad643e255ced19831d954972a196bc314a7d586
Felix <137139123+felixjr2121@users.noreply.github.com>
2023-09-20 18:59:05 -0500

    Print results of playRound to the page
    Creation and appending of "p" elements that contain the results of the
    round.

--- script.js
+++ script.js
@@ -5,47 +5,40 @@ function getComputerChoice() {
 }
 
 function playRound(playerChoice, computerChoice) {
+    const container = document.querySelector("#container");
+    const para = document.createElement('p');
     switch (true) {
         case (playerChoice == "rock" && computerChoice == "scissors"):
-            console.log("You won! Rock beats scissors!");
+            para.textContent = "You won! Rock beats scissors!";
             break;
 
         case (playerChoice == "paper" && computerChoice == "rock"):
-            console.log("You won! Paper beats rock!");
+            para.textContent = "You won! Paper beats rock!";
             break;
 
         case (playerChoice == "scissors" && computerChoice == "paper"):
-            console.log("You won! Scissors beats paper!");
+            para.textContent = "You won! Scissors beats paper!";
             break;
 
         case (playerChoice == "rock" && computerChoice == "paper"):
-            console.log("You lost! Rock loses to paper.");
+            para.textContent = "You lost! Rock loses to paper.";
             break;
 
         case (playerChoice == "paper" && computerChoice == "scissors"):
-            console.log("You lost! Paper loses to scissors.");
+            para.textContent = "You lost! Paper loses to scissors.";
             break;
 
         case (playerChoice == "scissors" && computerChoice == "rock"):
-            console.log("You lost! Scissors loses to rock.");
+            para.textContent = "You lost! Scissors loses to rock.";
             break;
 
         case (playerChoice == computerChoice):
-            console.log("It's a tie!");
+            para.textContent = "It's a tie!";
             break;
     }
+    container.appendChild(para);
 }
 
-/*function getPlayerChoice(event) {
-    let playerChoice;
-    if (event.target.classList.contains("rock")) {
-        playerChoice = "rock";
-    } else if (event.target.classList.contains("paper")) {
-        playerChoice = "paper";
-    } else if (event.target.classList.contains("scissors")) {
-        playerChoice = "scissors";
-    }
-}*/
 
 const buttons = document.querySelectorAll("button");
 
